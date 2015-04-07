15990d4e52c6987c5e9dfff5912cb7b74d9db36d
Pascal Pfiffner <phase.of.matter@gmail.com>
2015-04-07 11:54:06 -0400

    More solid R plotting
    - takes into account missing days
    - start/end date boundaries more flexible

--- appannie.R
+++ appannie.R
@@ -4,6 +4,17 @@
 runavg_n = 7
 runavg <- function(x, n=runavg_n) { filter(x, rep(1/n, n), sides=2) }
 
+# create data.frame (must have a Date column called "stamp") for a given year
+fullyear_start = 0
+fullyear <- function(x, date_end, decreasing=F) {
+	start = date_end - 3600*24*(365+runavg_n)
+	year = subset(x, stamp <= date_end & stamp > start)
+	ordered = year[with(year, order(stamp, decreasing=decreasing)),]
+	latestyear = max(ordered$stamp$year)
+	ordered$nday = ifelse(ordered$stamp$year < latestyear, ordered$stamp$yday, ordered$stamp$yday + 365)
+	return(ordered)
+}
+
 to_file = !interactive()
 
 for (f in Sys.glob('Numbers *.csv')) {
@@ -18,32 +29,40 @@ for (f in Sys.glob('Numbers *.csv')) {
 	d = read.csv(f)
 	d$stamp = strptime(d$date, "%Y-%m-%d")
 	
-	# past year and year before that and the one before that
-	y1 = d[(365+runavg_n):1,]
-	y2 = d[(730+runavg_n):366,]
-	y3 = d[(1095+runavg_n):731,]
+	# create ranges for the last 3 years; reverse order to sort by date from past to now
+	latest = max(d$stamp)
+	y1 = fullyear(d, latest)
+	y2 = fullyear(d, latest - 3600*24*365)
+	y3 = fullyear(d, latest - 3600*24*730)
 	max_d = max(y1$num_downloads, y2$num_downloads, y3$num_downloads, na.rm=T)
 	
-	lbl_set = subset(y1, stamp$mday == 1)
-	lbls = strftime(lbl_set$stamp, "%b")
-	lbls_at = nrow(y1) - as.numeric(rownames(lbl_set))
+	# create labels for the first entry of a month (entry from day 1 may be missing, so can't use stamp$mday == 1)
+	y1$mth = strftime(y1$stamp, "%b")
+	y1$prevmth = c(rep(NA,1),head(y1$mth,-1))
+	lbl_set = y1[y1$mth != y1$prevmth,]
 	
 	# daily downloads
 	par(mar=c(3,5,3,3))
-	plot(y3$num_downloads, type='l', main=n, xlab=NULL, xaxt='n', ylab="downloads / day", col='cornsilk2', ylim=c(0, max_d))
-	axis(1, at=lbls_at, labels=lbls)
-	abline(v=nrow(y1) - as.numeric(rownames(subset(y1, stamp$yday == 0))))
+	plot(y1$nday, y1$num_downloads, type='l', main=n, xlab=NULL, xaxt='n', ylab="downloads / day", col='cornsilk4', ylim=c(0, max_d))
+	axis(1, at=lbl_set$nday, labels=lbl_set$mth)
 	
-	lines(runavg(y3$num_downloads), lwd=2, col='cornflowerblue')
-	lines(y2$num_downloads, col='cornsilk3')
-	lines(runavg(y2$num_downloads), lwd=3, col='blue')
-	lines(y1$num_downloads, col='cornsilk4')
-	lines(runavg(y1$num_downloads), lwd=4, col='blue4')
+	# new year marker
+	abline(v=y1[y1$stamp$yday == 0,]$nday)
+	
+	if (nrow(y3) > 0) {
+		lines(y3$nday, y3$num_downloads, col='cornsilk2')
+		lines(y3$nday, runavg(y3$num_downloads), lwd=2, col='cornflowerblue')
+	}
+	if (nrow(y2) > 0) {
+		lines(y2$nday, y2$num_downloads, col='cornsilk3')
+		lines(y2$nday, runavg(y2$num_downloads), lwd=3, col='blue')
+	}
+	lines(y1$nday, runavg(y1$num_downloads), lwd=4, col='blue4')
 	
 	# number of updates (scaled)
 	max_upd = max(d$num_updates, na.rm=T)
 	factor = max_upd / max_d
-	lines(d[(365+runavg_n):1,]$num_updates / factor, lty='dotted')
+	lines(y1$nday, y1$num_updates / factor, lty='dotted')
 	axis(4, at=c(0, max_d), labels=c(0, max_upd))
 	
 	# legend
